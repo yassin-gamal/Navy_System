985a5fcaeb5aedb699d76491c8c8f58a381f9946
Helal Ismail <helal@surveypocket.com>
2013-04-24 14:36:13 +0200

    Update Helal

--- Navy_System/app/controllers/users_controller.rb
+++ Navy_System/app/controllers/users_controller.rb
@@ -25,7 +25,9 @@ class UsersController < ApplicationController
   # GET /users/new.json
   def new
     @user = User.new
-
+    @groups = Group.all
+    @ranks = Rank.all
+    
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @user }
@@ -44,7 +46,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to users_path, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
