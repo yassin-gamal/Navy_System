d3998fdda0da6a6b0abcd1c50de66156e82afc80
Helal Ismail <helal@surveypocket.com>
2013-04-27 13:41:52 +0200

    h

---
+++ Navy_System/app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  attr_accessible :address, :blood, :group_id, :job_type, :military_number, :name, :near_relative, :note, :password, :password_confirmation, :phone, :rank_id, :start_date, :status, :tag_date, :tas_date, :type
+  
+  
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+
+  belongs_to :rank
+  belongs_to :group
+
+  has_many :vacations
+  has_many :vacation_types, :through => :vacations
+end
